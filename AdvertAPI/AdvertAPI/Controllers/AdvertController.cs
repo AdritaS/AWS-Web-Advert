8e3b333cc72245f0fc1f7376313c2ac633e37bbc
Adrita Sharma <adritasharma.developer@gmail.com>
2019-08-31 23:04:45 +0530

    db lock

---
+++ AdvertAPI/AdvertAPI/Controllers/AdvertController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvertAPI.Models;
+using AdvertAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvertAPI.Controllers
+{
+    [Route("api/adverts")]
+    [ApiController]
+    public class AdvertController : ControllerBase
+    {
+        private readonly IAdvertStorageService _advertStorageService;
+        public AdvertController(IAdvertStorageService advertStorageService)
+        {
+            _advertStorageService = advertStorageService;
+        }
+        [HttpPost]
+        [Route("create")]
+        public async Task<IActionResult> Create(AdvertModel model)
+        {
+            string recordId = null;
+            try
+            {
+                recordId = await _advertStorageService.Add(model);
+            }
+            catch(KeyNotFoundException keyEx)
+            {
+                return new NotFoundResult();
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+
+            }
+            return StatusCode(201, new AdvertResponse { Id = recordId });
+        }
+
+        [HttpPut]
+        [Route("confirm")]
+        public async Task<IActionResult> Confirm(ConfirmAdvertModel model)
+        {
+            string recordId = null;
+            try
+            {
+                await _advertStorageService.Confirm(model);
+            }
+            catch (KeyNotFoundException keyEx)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+
+            }
+            return new OkResult();
+        }
+    }
+}
---
+++ AdvertAPI/AdvertAPI/Models/AdvertModelDb.cs
@@ -0,0 +1,26 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvertAPI.Models
+{
+    [DynamoDBTable("Adverts")]
+    public class AdvertModelDb
+    {
+        [DynamoDBHashKey]
+        public string Id { get; set; }
+        [DynamoDBProperty]
+        public string Title { get; set; }
+        [DynamoDBProperty]
+        public string Description { get; set; }
+        [DynamoDBProperty]
+        public double Price { get; set; }
+        [DynamoDBProperty]
+        public DateTime CreationDate { get; set; }
+        [DynamoDBProperty]
+        public AdvertStatus Status { get; set; }
+
+    }
+}
